51abbf6b906db5602767423f2f101d7f2f15eea5
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-02-14 12:08:53 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function app(){
+    let msg = 'FIrst react compenent'
+    return <div>
+        <h1>This is the Header of the compent</h1>
+        <p>This is the header {msg}</p>
+    </div>
+}
+
+export default app;
