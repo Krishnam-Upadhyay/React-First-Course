a8f051dd591bcdbef91099382ceab5d391249903
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-02-27 06:58:36 +0530

    Rendered my first app component
     I created a component and rendered it

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+function App(){
+   
+    return <p>This is First  Component</p>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import { React } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+
+// 
+// const header = <h1>This is the header of the app</h1>
+
+// ReactDOM.render(header,document.getElementById('root'));
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(<App/>);
