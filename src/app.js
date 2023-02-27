cced6258d592f45858d8c4cb74bfc1633a491209
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-02-27 07:26:20 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 
 function App(){
+    /* const p = document.createElement('p');
+    p.textContent = 'This is a demo paragraph';
    
-    return <p>This is First  Component</p>
+    return p; */
+
+     let msg = 'awesome';
+     /*
+    return <p>This is my first ParaGraph {msg}</p> */
+    return (<div>
+        <h1>THis is my Header</h1>
+        <p>This is my first ParaGraph {msg}</p>
+    </div>)
 }
 
 export default App;
