b5d1c6bd8d5cbc5f504185dc4865c2462aa6ff5d
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2023-02-27 07:26:58 +0530

    Delete src directory

--- src/app.js
+++
@@ -1,9 +0,0 @@
-function app(){
-    let msg = 'FIrst react compenent'
-    return <div>
-        <h1>This is the Header of the compent</h1>
-        <p>This is the header {msg}</p>
-    </div>
-}
-
-export default app;
